fix gradient

cosmos_rl/triton_ops/_compute_loss.py
@@ -2,7 +2,6 @@ import torch
 
 import triton
 import triton.language as tl
-from cosmos_rl.utils.logging import logger
 
 
 @triton.jit
@@ -70,18 +69,6 @@ def _compute_logprobs_forward(
     logsum_exp = tl.log(sum_exp_total) + max_logit
     tl.store(addr_of_sumexp, logsum_exp)
 
-    # [1, 1, vocab_size]
-    # [0, 0, index_of_logit]
-    # for i in range(0, vocab_size, BLOCK_SIZE):
-    #     offset = i + tl.arange(0, BLOCK_SIZE)
-    #     load_mask = offset < vocab_size
-    #     logit_of_current_token = tl.load(start_addr_of_token + offset, mask=load_mask, other=0).to(tl.float32)
-    #     logprob_of_current_token = logit_of_current_token - logsum_exp
-
-    #     store_mask = offset == index_of_logit
-    #     new_offset = tl.zeros_like(offset)
-    #     tl.store(addr_of_logprobs + new_offset, logprob_of_current_token, mask=store_mask)
-
     logit_of_current_token = tl.load(start_addr_of_token + index_of_logit).to(
         tl.float32
     )  # scalar
@@ -117,7 +104,7 @@ def _compute_logprobs_backward(
     bsz_cu_seqlen = tl.load(cu_seqlens + bsz_idx)
 
     addr_of_sumexp = sumexp_ptr + bsz_cu_seqlen + token_idx - seqlen_start_idx
-    sum_exp_total = tl.load(addr_of_sumexp)
+    logsum_exp = tl.load(addr_of_sumexp)
 
     start_addr_of_token = (
         full_logits_ptr + bsz_idx * max_len * vocab_size + token_idx * vocab_size
@@ -126,7 +113,7 @@ def _compute_logprobs_backward(
     addr_of_gradient_output = (
         gradient_output_ptr + bsz_cu_seqlen + token_idx - seqlen_start_idx
     )
-    gradient_output = tl.load(
+    gradient_output_of_single_logit = tl.load(
         addr_of_gradient_output
     )  # scalar, gradient of the logit with index `index_of_logit`
 
@@ -145,23 +132,28 @@ def _compute_logprobs_backward(
 
         # recompute the output of log_softmax
         x = tl.load(
-            start_addr_of_token + i + tl.arange(0, BLOCK_SIZE),
+            start_addr_of_token + offset,
             mask=mask,
             other=0,
         ).to(tl.float32)
-        log_softmax_output = x - sum_exp_total
+        log_softmax_output = x - logsum_exp
         exp_log_softmax_output = tl.exp(log_softmax_output)
-
+        # https://math.stackexchange.com/questions/4258008/derivative-of-the-log-softmax-function
+        # https://stackoverflow.com/questions/35304393/trying-to-understand-code-that-computes-the-gradient-wrt-to-the-input-for-logsof
         # gradient_x_i = gradient_output_i - exp(o_i) * sum_j(gradient_output_j)
         gradient_output_block = tl.zeros((BLOCK_SIZE,), dtype=tl.float32)
-        tl.where(offset == index_of_logit, gradient_output, gradient_output_block)
-
-        gradient_input_block = (
-            gradient_output_block - exp_log_softmax_output * gradient_output
+        gradient_output_block = tl.where(
+            offset == index_of_logit,
+            gradient_output_of_single_logit,
+            gradient_output_block,
         )
 
+        gradient_input_block = (
+            gradient_output_block
+            - exp_log_softmax_output * gradient_output_of_single_logit
+        )
         tl.store(
-            addr_of_gradient_full_logits + i + tl.arange(0, BLOCK_SIZE),
+            addr_of_gradient_full_logits + offset,
             gradient_input_block,
             mask=mask,
         )
@@ -177,11 +169,6 @@ class TritonComputeLogprobs(torch.autograd.Function):
         assert (
             full_logits.shape[:2] == input_ids.shape[:2]
         ), "full_logits should have the same shape as input_ids"
-
-        shift_input_ids = torch.empty_like(input_ids)
-        shift_input_ids[:, :-1] = input_ids[:, 1:]
-        shift_input_ids[:, -1] = 0
-
         n_logprob_tokens = logprob_masks.sum()
         logprobs = torch.empty(
             n_logprob_tokens, dtype=full_logits.dtype, device=full_logits.device
@@ -196,9 +183,6 @@ class TritonComputeLogprobs(torch.autograd.Function):
         seqlen_end_idx = (
             int_logprob_masks.size(1) - 1 - reversed_int_logprob_masks.argmax(dim=-1)
         )
-
-        logger.info(f"LMS: seqlen_start_idx: {seqlen_start_idx}")
-        logger.info(f"LMS: seqlen_end_idx: {seqlen_end_idx}")
 
         sum_exp = torch.zeros(
             (n_logprob_tokens), dtype=torch.float32, device=full_logits.device
@@ -220,7 +204,6 @@ class TritonComputeLogprobs(torch.autograd.Function):
             vocab_size,
             BLOCK_SIZE=BLOCK_SIZE,
         )
-        logger.info(f"LMS: sum_exp: {sum_exp}")
         ctx.save_for_backward(
             input_ids,
             full_logits,
@@ -233,8 +216,6 @@ class TritonComputeLogprobs(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output, grad_cu_seqlens):
-        # https://math.stackexchange.com/questions/4258008/derivative-of-the-log-softmax-function
-        # https://stackoverflow.com/questions/35304393/trying-to-understand-code-that-computes-the-gradient-wrt-to-the-input-for-logsof
         (
             input_ids,
             full_logits,

cosmos_rl/utils/util.py
@@ -46,6 +46,7 @@ import functools
 from cosmos_rl.utils.logging import logger
 from safetensors import safe_open
 from cosmos_rl.utils.constant import CACHE_DIR
+from cosmos_rl.triton_ops._compute_loss import TritonComputeLogprobs
 
 
 def create_cached_dir_if_needed():
@@ -407,9 +408,6 @@ def selective_log_softmax(logits, index):
             ).squeeze(-1)
             per_token_logps.append(row_per_token_logps)
         per_token_logps = torch.stack(per_token_logps)
-    logger.info(
-        f"LMS: shape of per_token_logps: {per_token_logps.shape}, value: {per_token_logps}"
-    )
     return per_token_logps
 
 
@@ -980,6 +978,7 @@ def create_async_task(coro):
 def compute_logprobs(
     minibatch: Dict[str, Any],
     full_logits: torch.Tensor,
+    use_triton: bool = True,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     """
     Compute the per-token log probabilities and advantages
@@ -1011,5 +1010,11 @@ def compute_logprobs(
     assert (
         full_logits.shape[:2] == shifted_input_ids.shape[:2]
     ), f"Logits shape {full_logits.shape} does not match input_ids shape {shifted_input_ids.shape}"
-    logps = selective_log_softmax(full_logits, shifted_input_ids) * logprob_masks
-    return logps, logprob_masks
+    if use_triton:
+        loss, cu_seqlens = TritonComputeLogprobs.apply(
+            shifted_input_ids, logprob_masks, full_logits
+        )
+        return loss, cu_seqlens
+    else:
+        logps = selective_log_softmax(full_logits, shifted_input_ids) * logprob_masks
+        return logps, logprob_masks

tests/test_triton.py
@@ -3,17 +3,15 @@ import torch
 from typing import List, Dict, Any, Tuple
 
 from cosmos_rl.utils.util import compute_logprobs
-from cosmos_rl.policy.config import Config as CosmosConfig
-from cosmos_rl.policy.config import GrpoConfig
-from cosmos_rl.triton_ops._compute_loss import TritonComputeLogprobs
 
-# PROMPT_LEN_MAX = 1024
-# COMPLETION_LEN_MAX = 4096
-# VOCAB_SIZE = 152064
+PROMPT_LEN_MAX = 1024
+COMPLETION_LEN_MAX = 4096
+VOCAB_SIZE = 152064
 
-PROMPT_LEN_MAX = 128
-COMPLETION_LEN_MAX = 256
-VOCAB_SIZE = 1024
+# Below are only for kernel debugging
+# PROMPT_LEN_MAX = 1
+# COMPLETION_LEN_MAX = 4
+# VOCAB_SIZE = 8
 
 
 def simulate_samples(
@@ -34,12 +32,12 @@ def simulate_samples(
     # generate prompt_ids and completion_ids
     for prompt_len in prompt_lens:
         prompt_ids.append(
-            [random.randint(1, VOCAB_SIZE - 1) for _ in range(prompt_len)]
+            [random.randint(1, VOCAB_SIZE - 2) for _ in range(prompt_len)]
         )
 
     for completion_len in completion_lens:
         completion_ids.append(
-            [random.randint(1, VOCAB_SIZE - 1) for _ in range(completion_len)]
+            [random.randint(1, VOCAB_SIZE - 2) for _ in range(completion_len)]
         )
 
     # generate logprob_masks and input_ids
@@ -65,7 +63,9 @@ def simulate_samples(
 
 
 def simulate_pad_samples(
-    samples: List[Tuple[List[int], List[int]]], max_len: int, pad_token_id: int = 1
+    samples: List[Tuple[List[int], List[int]]],
+    max_len: int,
+    pad_token_id: int = VOCAB_SIZE - 1,
 ) -> torch.Tensor:
     """
     Pad samples to the same length. simulate `policy_collate_fn` in DataPacker.
@@ -117,67 +117,53 @@ def simulate_generate_mini_batch(
 
 
 def compute_normal_logprobs(mini_batch, full_logits):
-    logps, logprob_masks = compute_logprobs(mini_batch, full_logits)
+    logps, logprob_masks = compute_logprobs(mini_batch, full_logits, use_triton=False)
     return logps, logprob_masks
 
 
-def compute_triton_logprobs(mini_batch, full_logits):
-    triton_logprobs, cu_seqlens = TritonComputeLogprobs.apply(
-        mini_batch["input_ids"],
-        mini_batch["logprob_masks"],
-        full_logits,
-    )
-    return triton_logprobs, cu_seqlens
-
-
-def test_computing_logprobs_and_loss():
-    # instantiate a GRPOTrainer
-    cosmos_config = CosmosConfig()
-    cosmos_config.train.train_policy = GrpoConfig()
-
-    bsz = 6
-
+def test_computing_logprobs_and_loss(bsz: int = 6):
     samples = simulate_samples(bsz=bsz, n_ignore_prefix_tokens=0)
     mini_batch, max_len_from_samples = simulate_generate_mini_batch(samples)
 
+    logprob_masks = mini_batch["logprob_masks"]
+
     # now generate the full logits
     full_logits = torch.randn(bsz, max_len_from_samples, VOCAB_SIZE).cuda().bfloat16()
-    # triton_full_logits = full_logits.clone()
+
+    triton_full_logits = full_logits.clone()
+    triton_full_logits.requires_grad = True
     triton_mini_batch = {}
     for k, v in mini_batch.items():
         triton_mini_batch[k] = v.clone()
 
-    print("Normal")
     full_logits.requires_grad = True
     # compute logprobs
-
     normal_logps, logprob_masks = compute_normal_logprobs(mini_batch, full_logits)
+    selected_normal_logprobs = torch.masked_select(normal_logps, logprob_masks)
 
-    normal_mean = normal_logps.mean()
-    print(f"normal_mean: {normal_mean}")
+    normal_mean = selected_normal_logprobs.mean()
     normal_mean.backward()
 
-    print("Triton")
     # Triton forward
-    triton_logprobs, cu_seqlens = compute_triton_logprobs(
-        mini_batch,
-        full_logits,
+    triton_logprobs, cu_seqlens = compute_logprobs(
+        triton_mini_batch,
+        triton_full_logits,
+        use_triton=True,
     )
 
     triton_mean = triton_logprobs.mean()
     triton_mean.backward()
 
-    for i in range(bsz):
-        batch_i_logprobs_normal = normal_logps[i]  # [seqlen,]
-        mask_of_i = logprob_masks[i]  # [seqlen,]
-        normal_logprobs_of_i = torch.masked_select(batch_i_logprobs_normal, mask_of_i)
+    assert torch.allclose(normal_mean, triton_mean)
 
-        start_idx = cu_seqlens[i]
-        end_idx = cu_seqlens[i + 1]
-        batch_i_logprobs_triton = triton_logprobs[start_idx:end_idx]
+    # Forward compare
+    assert torch.allclose(selected_normal_logprobs, triton_logprobs)
 
-        assert normal_logprobs_of_i.numel() == batch_i_logprobs_triton.numel()
-        # assert torch.allclose(normal_logprobs_of_i, batch_i_logprobs_triton)
+    # backward compare
+    # Gradient values are small, so we lossen the tolerance
+    assert torch.allclose(
+        full_logits.grad, triton_full_logits.grad, atol=1e-3, rtol=1e-2
+    )
 
 
 if __name__ == "__main__":