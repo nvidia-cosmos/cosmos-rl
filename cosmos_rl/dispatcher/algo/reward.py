7bbe9adeaf4c1948db5f79a860249bd2b28231e1
huik <huik@nvidia.com>
2025-07-15 06:32:02 -0700

    fix

--- cosmos_rl/dispatcher/algo/reward.py
+++ cosmos_rl/dispatcher/algo/reward.py
@@ -204,7 +204,7 @@ def boxed_math_reward_fn(
         return score
     except TimeoutException as e:
         logger.error(
-            f"TimeoutException: {e} with \nreference={reference}\nto_be_evaluated={to_be_evaluated}"
+            f"Caught TimeoutException: {e}\nreference={reference}\nto_be_evaluated={to_be_evaluated}"
         )
         return 0.0
     except Exception:
