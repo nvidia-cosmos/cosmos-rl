5a01d9193acf92984915ec12ad51ec05895b2f6a
jingxuz <jingxuz@nvidia.com>
2025-08-11 04:08:26 +0000

    fix: lint the code

--- cosmos_rl/rollout/vllm_rollout/vllm_rollout_worker.py
+++ cosmos_rl/rollout/vllm_rollout/vllm_rollout_worker.py
@@ -1348,6 +1348,7 @@ class vLLMRolloutWorker(RolloutWorkerBase):
                 if self.rollout.rollout_config.multi_turn_config.enable:
                     # HACK(zjx): always report the multi-turn completions to the controller
                     should_report = True
+                    # TODO(zjx): write completions for statistics the completion token counts
                     valid_completions = [[""] for _ in range(len(completions))]
                 else:
                     # Remove empty completions
@@ -1411,7 +1412,6 @@ class vLLMRolloutWorker(RolloutWorkerBase):
                         )
                         and len(valid_completions) > 0
                     )
-
 
                 if should_report:
                     url_suffix = COSMOS_API_ROLLOUT_SUFFIX
