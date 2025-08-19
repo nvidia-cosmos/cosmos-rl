c21769c643ca90d13787efb5d03e07ea83b86a55
jiaxinc <jiaxinc@nvidia.com>
2025-08-18 20:22:50 -0700

    support direct sft dataset passing

--- cosmos_rl/policy/model/qwen2_5_vl/__init__.py
+++ cosmos_rl/policy/model/qwen2_5_vl/__init__.py
@@ -43,15 +43,12 @@ from functools import cached_property
 import cosmos_rl.policy.kernel.modeling_utils as modeling_utils
 from cosmos_rl.policy.kernel.norm import RMSNorm
 from cosmos_rl.policy.kernel.fused import MLPActMulFunc
-<<<<<<< HEAD
 from cosmos_rl.utils.sequence_packing import pack_sequences_for_inputs
-=======
-from cosmos_rl.policy.model.qwen2_5_vl.encoder import (
+from cosmos_rl.policy.model.vision_encoder.qwen2_5_vl import (
     Qwen2_5_VL_Encoder_Args,
     Qwen2_5_VisionTransformerPretrainedModel,
     rotate_half,
 )
->>>>>>> 801b637 (extract vision_encoder code to single file)
 
 
 @dataclass
