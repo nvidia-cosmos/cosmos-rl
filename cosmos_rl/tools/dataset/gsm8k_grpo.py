9b076eedd9fad5ab90b0a3eb5a0519e568d5bbfe
bob <maoshengl@nvidia.com>
2025-08-20 07:57:01 +0000

    change dataset back

--- cosmos_rl/tools/dataset/gsm8k_grpo.py
+++ cosmos_rl/tools/dataset/gsm8k_grpo.py
@@ -44,7 +44,7 @@ class GSM8kDataset(Dataset):
             self.dataset = modelscope_dataset_if_enabled
 
     def __len__(self):
-        return 1024
+        return len(self.dataset)
 
     def __getitem__(self, idx: int) -> tuple[str, str]:
         """
