b1ba7b39ac30f730753e77c9b6d9da06cb4b042b
huik <huik@nvidia.com>
2025-08-29 23:05:21 -0700

    update

--- tests/test_hf_models.py
+++ tests/test_hf_models.py
@@ -64,8 +64,8 @@ class TestHFModel(unittest.TestCase):
         for model_id in [
             "Qwen/Qwen2.5-VL-7B-Instruct",
             "llava-hf/llava-1.5-7b-hf",
-            # "google/gemma-3-12b-it", Need access to the repo
-            "mistralai/Mistral-7B-Instruct-v0.3",
+            # "google/gemma-3-12b-it", # Need access to the repo
+            # "mistralai/Mistral-7B-Instruct-v0.3", # Need access to the repo
             "microsoft/phi-4",
         ]:
             for dtype in [torch.bfloat16, torch.float32]:
@@ -128,7 +128,7 @@ class TestHFModel(unittest.TestCase):
             "Qwen/Qwen2.5-VL-7B-Instruct",
             "llava-hf/llava-1.5-7b-hf",
             # "google/gemma-3-12b-it", # Need access to the repo
-            "mistralai/Mistral-7B-Instruct-v0.3",
+            # "mistralai/Mistral-7B-Instruct-v0.3", # Need access to the repo
             "microsoft/phi-4",
         ]:
             dtype = torch.bfloat16
