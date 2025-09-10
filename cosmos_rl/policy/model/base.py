nvidia-cosmos/cosmos-rl

commit 48c094b88ef57d6ef105bff32b85b60a0dcbfa9d

Refactor: reset named_buffers in load_hf_weights (#252)

cosmos_rl/policy/model/base.py
@@ -501,14 +501,15 @@ class ModelRegistry:
 
         model_cls = ModelRegistry._MODEL_REGISTRY[model_type]
 
+        cosmos_default_dtype = util.str2torch_dtype(
+            config.train.master_dtype
+            if config.train.master_dtype is not None
+            else config.train.param_dtype
+        )
+        hf_config.torch_dtype = cosmos_default_dtype
+
         with torch.device("meta"):
-            with util.cosmos_default_dtype(
-                util.str2torch_dtype(
-                    config.train.master_dtype
-                    if config.train.master_dtype is not None
-                    else config.train.param_dtype
-                )
-            ):
+            with util.cosmos_default_dtype(cosmos_default_dtype):
                 try:
                     model = model_cls.from_pretrained(
                         hf_config,

.github/workflows/build-and-test.yaml
@@ -67,6 +67,7 @@ jobs:
           ${DOCKER_RUN} python tests/test_data_packer.py
           ${DOCKER_RUN} python tests/test_sequence_packing.py
           ${LONG_DOCKER_RUN} python tests/test_integration.py --stream
+          ${DOCKER_RUN} python tests/test_hf_models.py
 
       - name: Cleanup
         if: always()

cosmos_rl/policy/model/hf_models/__init__.py
@@ -19,7 +19,6 @@ import inspect
 from typing import Tuple, List, Optional, Callable
 from transformers import AutoConfig
 from cosmos_rl.utils.util import (
-    sync_model_vocab,
     clear_weight_name,
     safe_deep_getattr,
     load_model_class_by_config,
@@ -271,80 +270,8 @@ class HFModel(BaseModel):
         return multi_modal_projector
 
     def post_to_empty_hook(self, cosmos_config: CosmosConfig):
-        # reset buffer registered in __init__() function,
-        # e.g. rotary_emb.inv_freq, embed_tokens.embed_scale
-        # For hf compatibility
-        language_model = (
-            getattr(self.language_model, "model", None) or self.language_model
-        )
-        rotary_emb = getattr(language_model, "rotary_emb", None) or getattr(
-            language_model, "rotary_pos_emb", None
-        )
-        current_device = torch.cuda.current_device()
-        if rotary_emb is not None:
-            rope_init_fn = getattr(rotary_emb, "rope_init_fn", None)
-            if rope_init_fn is not None:
-                inv_freq, rotary_emb.attention_scaling = rope_init_fn(
-                    self.text_config, device=current_device
-                )
-                rotary_emb.register_buffer("inv_freq", inv_freq, persistent=False)
-            else:
-                logger.warning(
-                    "rotary_emb does not have rope_init_fn, cannot reset inv_freq."
-                )
-        # Models like Gemma have rotary_emb_local
-        rotary_emb_local = getattr(language_model, "rotary_emb_local", None)
-        if rotary_emb_local is not None:
-            rope_init_fn = getattr(rotary_emb_local, "rope_init_fn", None)
-            if rope_init_fn is not None:
-                local_inv_freq, rotary_emb_local.attention_scaling = rope_init_fn(
-                    self.text_config, device=current_device
-                )
-                rotary_emb_local.register_buffer(
-                    "inv_freq", local_inv_freq, persistent=False
-                )
-            else:
-                logger.warning(
-                    "rotary_emb_local does not have rope_init_fn, cannot reset inv_freq."
-                )
-
-        if self.text_config.model_type in ["gemma3_text"]:
-            embed_tokens = language_model.embed_tokens
-            embed_scale = self.text_config.hidden_size**0.5
-            embed_tokens.register_buffer(
-                "embed_scale", torch.tensor(embed_scale), persistent=False
-            )
-
-        vision_model = self.vision_model
-        if vision_model is not None:
-            rotary_emb = getattr(vision_model, "rotary_pos_emb", None) or getattr(
-                vision_model, "rotary_emb", None
-            )
-            if rotary_emb is not None:
-                rope_init_fn = getattr(rotary_emb, "rope_init_fn", None)
-                if rope_init_fn is not None:
-                    inv_freq, rotary_emb.attention_scaling = rope_init_fn(
-                        self.vision_config, device=current_device
-                    )
-                    rotary_emb.register_buffer("inv_freq", inv_freq, persistent=False)
-            # CLIPVisionTransformer
-            embeddings = safe_deep_getattr(
-                vision_model, "vision_model.embeddings", None
-            )
-            # assert embeddings is not None, f"Can not get embeddings from {vision_model}"
-            if (
-                embeddings is not None
-                and getattr(embeddings, "position_ids", None) is not None
-                and getattr(embeddings, "num_positions", None) is not None
-            ):
-                position_ids = (
-                    torch.arange(embeddings.num_positions)
-                    .expand((1, -1))
-                    .to(current_device)
-                )
-                embeddings.register_buffer(
-                    "position_ids", position_ids, persistent=False
-                )
+        # Will reset all named buffers in load_hf_weights
+        return
 
     @property
     def parallelize_fn(self):
@@ -359,6 +286,16 @@ class HFModel(BaseModel):
         and moving each stage to a different device.
         """
         assert False, "Pipeline split is not supported for HFModel"
+
+    def reset_named_buffers(self, hf_model):
+        # copy named buffers from hf_model to self.model
+        hf_named_buffers = {k: v for k, v in hf_model.named_buffers()}
+        for name, cosmos_hf_buffer in self.model.named_buffers():
+            assert name in hf_named_buffers, f"Buffer {name} not found in hf model"
+            hf_buf = hf_named_buffers[name].to(
+                device=cosmos_hf_buffer.device, dtype=cosmos_hf_buffer.dtype
+            )
+            cosmos_hf_buffer.data.copy_(hf_buf.data)
 
     def load_hf_weights(
         self,
@@ -376,13 +313,18 @@ class HFModel(BaseModel):
             info_inly (bool): Only collect the tensor infomation without actual data loading.
         """
         model_type = self.hf_config.model_type
-        model_with_weights = self.model_class.from_pretrained(
+        dtype = self.hf_config.torch_dtype
+        self.model = self.model.to(dtype=dtype)
+        hf_model = self.model_class.from_pretrained(
             model_name_or_path,
             revision=revision,
+            config=self.hf_config,
             trust_remote_code=True,
-        ).to("cpu")
+        ).to(device="cpu", dtype=dtype)
 
-        state_dict = model_with_weights.state_dict()
+        self.reset_named_buffers(hf_model)
+
+        state_dict = hf_model.state_dict()
         self_state_dict = self.model.state_dict()
         self_state_dict = {clear_weight_name(k): v for k, v in self_state_dict.items()}
         all_tensor_names = self_state_dict.keys()
@@ -430,7 +372,7 @@ class HFModel(BaseModel):
                 ), f"Shape mismatch: {local_view.shape} != {shared_weight.shape} for {dest_name}"
                 with torch.no_grad():
                     local_view.data.copy_(shared_weight.to(device))
-        del model_with_weights
+        del hf_model
 
         # Enable gradient checkpointing
         if self._gradient_checkpointing_enabled:
@@ -594,7 +536,6 @@ class HFModel(BaseModel):
             max_position_embeddings = hf_config.max_position_embeddings
         else:
             hf_config.max_position_embeddings = max_position_embeddings
-        _ = sync_model_vocab(model_name_or_path)
 
         return cls.from_model_args(hf_config)
 

tests/test_hf_models.py
@@ -1,0 +1,235 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import copy
+import torch
+import unittest
+from contextlib import contextmanager
+from PIL import Image
+from qwen_vl_utils import process_vision_info
+from transformers import AutoConfig, AutoProcessor
+
+from cosmos_rl.utils.parallelism import ParallelDims
+from cosmos_rl.policy.model.hf_models import HFModel
+from cosmos_rl.policy.config import Config as CosmosConfig, ParallelismConfig
+
+
+@contextmanager
+def cosmos_default_dtype(dtype: torch.dtype):
+    old = torch.get_default_dtype()
+    torch.set_default_dtype(dtype)
+    try:
+        yield
+    finally:
+        torch.set_default_dtype(old)
+
+
+def test_hf_model_generate(model, inputs):
+    generation_kwargs = dict(
+        max_new_tokens=1,
+        output_logits=True,
+        return_dict_in_generate=True,
+    )
+    logits = model.generate(**inputs, **generation_kwargs).logits[0]
+    return logits
+
+
+def test_hf_model_forward(model, inputs):
+    with torch.no_grad():
+        logits = model(**inputs).logits
+        return logits[:, -1, :]
+
+
+def test_cosmos_hf_model(model, inputs):
+    with torch.no_grad():
+        logits = model(**inputs)
+        return logits[:, -1, :]
+
+
+class TestHFModel(unittest.TestCase):
+    def test_post_to_empty_hook(self):
+        for model_id in [
+            "Qwen/Qwen2.5-VL-7B-Instruct",
+            "llava-hf/llava-1.5-7b-hf",
+            # "google/gemma-3-12b-it", # Need access to the repo
+            # "mistralai/Mistral-7B-Instruct-v0.3", # Need access to the repo
+            "microsoft/phi-4",
+        ]:
+            for dtype in [torch.bfloat16, torch.float32]:
+                # To avoid out-of-memory issues, bypass float32 precision for models which have more than 10B parameters
+                if (
+                    model_id in ["google/gemma-3-12b-it", "microsoft/phi-4"]
+                    and dtype == torch.float32
+                ):
+                    continue
+                max_position_embeddings = 1024
+                # Load config
+                config = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
+                config.max_position_embeddings = max_position_embeddings
+                config.torch_dtype = dtype
+                # Load cosmos hf model
+                cosmos_hf_model = None
+                with torch.device("meta"):
+                    with cosmos_default_dtype(dtype):
+                        cosmos_hf_model = HFModel.from_pretrained(
+                            config,
+                            model_id,
+                            max_position_embeddings=max_position_embeddings,
+                        )
+                cosmos_hf_model.to_empty(device="cuda")
+                cosmos_hf_model.post_to_empty_hook(CosmosConfig())
+                parallel_dims = ParallelDims.from_config(ParallelismConfig(tp_size=1))
+                cosmos_hf_model.load_hf_weights(
+                    model_id, parallel_dims, "cuda", revision=None
+                )
+
+                # Load hf model
+                hf_model = cosmos_hf_model.model_class.from_pretrained(
+                    model_id, trust_remote_code=True, config=config
+                ).to("cuda", dtype=dtype)
+                hf_named_buffers = {k: v for k, v in hf_model.named_buffers()}
+
+                for name, cosmos_hf_buffer in cosmos_hf_model.model.named_buffers():
+                    assert (
+                        name in hf_named_buffers
+                    ), f"Buffer {name} not found in hf model"
+                    hf_buffer = hf_named_buffers[name]
+                    assert (
+                        cosmos_hf_buffer.shape == hf_buffer.shape
+                    ), f"Shape mismatch: {cosmos_hf_buffer.shape} != {hf_buffer.shape} for {name}"
+                    assert (
+                        cosmos_hf_buffer.dtype == hf_buffer.dtype
+                    ), f"Dtype mismatch: {cosmos_hf_buffer.dtype} != {hf_buffer.dtype} for {name}"
+                    assert torch.equal(
+                        cosmos_hf_buffer, hf_buffer
+                    ), f"Buffer {name} is not equal to the one in hf model"
+
+                print(f"{model_id} with {dtype=} post_to_empty_hook test passed.")
+                del cosmos_hf_model
+                del hf_model
+                del hf_named_buffers
+                torch.cuda.empty_cache()
+
+    def test_forward(self):
+        for model_id in [
+            "Qwen/Qwen2.5-VL-7B-Instruct",
+            "llava-hf/llava-1.5-7b-hf",
+            # "google/gemma-3-12b-it", # Need access to the repo
+            # "mistralai/Mistral-7B-Instruct-v0.3", # Need access to the repo
+            "microsoft/phi-4",
+        ]:
+            dtype = torch.bfloat16
+            max_position_embeddings = 4096
+            config = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
+            config.max_position_embeddings = max_position_embeddings
+            config._attn_implementation = "eager"
+            config.torch_dtype = dtype
+            cosmos_hf_model = None
+            with torch.device("meta"):
+                with cosmos_default_dtype(dtype):
+                    cosmos_hf_model = HFModel.from_pretrained(
+                        config,
+                        model_id,
+                        max_position_embeddings=max_position_embeddings,
+                    )
+            cosmos_hf_model.to_empty(device="cuda")
+            cosmos_hf_model.post_to_empty_hook(CosmosConfig())
+            parallel_dims = ParallelDims.from_config(ParallelismConfig(tp_size=1))
+            cosmos_hf_model.load_hf_weights(
+                model_id, parallel_dims, "cuda", revision=None
+            )
+
+            hf_model = cosmos_hf_model.model_class.from_pretrained(
+                model_id, trust_remote_code=True, config=config
+            ).to("cuda", dtype=dtype)
+            processor = AutoProcessor.from_pretrained(
+                model_id, trust_remote_code=True, use_fast=True
+            )
+            try:
+                if cosmos_hf_model.is_vlm:
+                    current_dir = os.path.dirname(os.path.abspath(__file__))
+                    image = Image.open(
+                        os.path.join(current_dir, "data", "test_hf_model.jpg")
+                    )
+                    messages = [
+                        [
+                            {
+                                "role": "user",
+                                "content": [
+                                    {
+                                        "type": "image",
+                                        "image": image,
+                                    },
+                                    {"type": "text", "text": "describe the image"},
+                                ],
+                            }
+                        ]
+                    ]
+                    text = processor.apply_chat_template(messages, tokenize=False)
+                    image_inputs, video_inputs = process_vision_info(messages)
+
+                    inputs = processor(
+                        text=text,
+                        images=image_inputs,
+                        videos=video_inputs,
+                        padding=True,
+                        return_tensors="pt",
+                    ).to("cuda")
+                else:
+                    messages = [
+                        {
+                            "role": "system",
+                            "content": "You are a pirate chatbot who always responds in pirate speak!",
+                        },
+                        {"role": "user", "content": "Who are you?"},
+                    ]
+                    text = processor.apply_chat_template(messages, tokenize=False)
+                    inputs = processor(
+                        text=text,
+                        return_tensors="pt",
+                    ).to("cuda")
+
+                hf_generate_logits = test_hf_model_generate(
+                    hf_model, copy.deepcopy(inputs)
+                )
+                hf_forward_logits = test_hf_model_forward(
+                    hf_model, copy.deepcopy(inputs)
+                )
+                cosmos_hf_logits = test_cosmos_hf_model(
+                    cosmos_hf_model, copy.deepcopy(inputs)
+                )
+                assert torch.equal(
+                    hf_generate_logits, hf_forward_logits
+                ), f"{hf_generate_logits} != {hf_forward_logits}"
+                assert torch.equal(
+                    hf_generate_logits, cosmos_hf_logits
+                ), f"{hf_generate_logits} != {cosmos_hf_logits}"
+
+                print(f"{model_id} forward test passed.")
+                del cosmos_hf_model
+                del hf_model
+                del cosmos_hf_logits
+                del hf_generate_logits
+                del hf_forward_logits
+                torch.cuda.empty_cache()
+            except Exception as e:
+                print(f"{model_id} forward test failed.")
+                print(f"{e}")
+                exit(-1)
+
+
+if __name__ == "__main__":
+    unittest.main()

commit 551295be773bcc8da134c5e1d7ab17ea70a0adfa

Fix the bug introduced in PR https://github.com/nvidia-cosmos/cosmos-rl/pull/252

cosmos_rl/policy/model/base.py
@@ -506,7 +506,6 @@ class ModelRegistry:
             if config.train.master_dtype is not None
             else config.train.param_dtype
         )
-        hf_config.torch_dtype = cosmos_default_dtype
 
         with torch.device("meta"):
             with util.cosmos_default_dtype(cosmos_default_dtype):