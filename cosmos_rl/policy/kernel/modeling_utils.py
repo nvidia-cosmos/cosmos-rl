dec90a5117d43e8e73aa76402fe1fa0882b161ae
lms <maoshengl@nvidia.com>
2025-09-25 13:24:48 +0000

    add tests

--- cosmos_rl/policy/kernel/modeling_utils.py
+++ cosmos_rl/policy/kernel/modeling_utils.py
@@ -26,18 +26,30 @@ def decide_fa_version():
     return 2
 
 
-class FlashAttnMeta:
-    _flash_attn_meta = None
+class FlashAttnMetaSingleton(type):
+    _instances = {}
 
-    def __init__(self):
-        self.fa_version = decide_fa_version()
-        if self.fa_version == 3:
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(FlashAttnMetaSingleton, cls).__call__(
+                *args, **kwargs
+            )
+        return cls._instances[cls]
+
+
+class FlashAttnMeta(metaclass=FlashAttnMetaSingleton):
+    def __init__(self, torch_compile: bool = True):
+        # FA3 is not compatible with torch.compile
+        # The support is in WIP: https://github.com/Dao-AILab/flash-attention/pull/1769
+        if decide_fa_version() == 3 and not torch_compile:
             try:
                 # Just as a check to see if flash_attn_3 is installed
                 import flash_attn_3  # noqa: F401
 
                 # According to: https://github.com/Dao-AILab/flash-attention/blob/add175637c5d54b74bc25372e49ce282d6f236fc/README.md?plain=1#L62
                 from flash_attn_interface import flash_attn_func, flash_attn_varlen_func
+
+                self.fa_version = 3
             except ImportError:
                 logger.warning(
                     "FlashAttention3 is not installed. Using FlashAttention2 instead."
@@ -45,6 +57,7 @@ class FlashAttnMeta:
                 self.fa_version = 2
                 from flash_attn import flash_attn_func, flash_attn_varlen_func
         else:
+            self.fa_version = 2
             from flash_attn import flash_attn_func, flash_attn_varlen_func
         logger.info(f"[Cosmos-RL] Using FlashAttention-{self.fa_version}.")
         self.flash_attn_func = flash_attn_func
@@ -52,11 +65,6 @@ class FlashAttnMeta:
         from flash_attn.layers.rotary import apply_rotary_emb as ori_apply_rotary_emb
 
         self.apply_rotary_emb = ori_apply_rotary_emb
-
-    def __new__(cls):
-        if cls._flash_attn_meta is None:
-            cls._flash_attn_meta = super().__new__(cls)
-        return cls._flash_attn_meta
 
     def set_deterministic(self, deterministic: bool):
         self.flash_attn_func = partial(
@@ -67,6 +75,6 @@ class FlashAttnMeta:
         )
 
 
-def init_flash_attn_meta(deterministic: bool = False):
-    FlashAttnMeta().set_deterministic(deterministic)
-    logger.info(f"[Cosmos-RL] FlashAttnMeta initialized to {FlashAttnMeta()}.")
+def init_flash_attn_meta(deterministic: bool = False, compile: bool = True):
+    FlashAttnMeta(torch_compile=compile).set_deterministic(deterministic)
+    assert FlashAttnMeta() is FlashAttnMeta(), "FlashAttnMeta is not a singleton"
