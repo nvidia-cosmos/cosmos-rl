573f6a440301cf3ce9c980c8df52f28173e6e858
huik <huik@nvidia.com>
2025-09-25 08:58:04 -0700

    add qwen3_vl_moe

--- cosmos_rl/dispatcher/data/packer/hf_vlm_data_packer.py
+++ cosmos_rl/dispatcher/data/packer/hf_vlm_data_packer.py
@@ -197,6 +197,172 @@ class HFVLMDataPacker(DataPacker):
         # no match found
         return False, token_ids, label_ids
 
+    def _get_rope_index(
+        self,
+        input_ids: Optional[torch.LongTensor] = None,
+        image_grid_thw: Optional[torch.LongTensor] = None,
+        video_grid_thw: Optional[torch.LongTensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        """Different from the original implementation, Qwen3VLMoe use timestamps rather than absolute time position ids."""
+
+        # Since we use timestamps to seperate videos, like <t1> <vision_start> <frame1> <vision_end> <t2> <vision_start> <frame2> <vision_end>, the video_grid_thw should also be split
+        if video_grid_thw is not None:
+            video_grid_thw = torch.repeat_interleave(
+                video_grid_thw, video_grid_thw[:, 0], dim=0
+            )
+            video_grid_thw[:, 0] = 1
+
+        spatial_merge_size = self.hf_config.vision_config.spatial_merge_size
+        image_token_id = self.hf_config.image_token_id
+        video_token_id = self.hf_config.video_token_id
+        vision_start_token_id = self.hf_config.vision_start_token_id
+        mrope_position_deltas = []
+        if input_ids is not None and (
+            image_grid_thw is not None or video_grid_thw is not None
+        ):
+            total_input_ids = input_ids
+            if attention_mask is None:
+                attention_mask = torch.ones_like(total_input_ids)
+            position_ids = torch.ones(
+                3,
+                input_ids.shape[0],
+                input_ids.shape[1],
+                dtype=input_ids.dtype,
+                device=input_ids.device,
+            )
+            image_index, video_index = 0, 0
+            attention_mask = attention_mask.to(total_input_ids.device)
+            for i, input_ids in enumerate(total_input_ids):
+                input_ids = input_ids[attention_mask[i] == 1]
+                image_nums, video_nums = 0, 0
+                vision_start_indices = torch.argwhere(
+                    input_ids == vision_start_token_id
+                ).squeeze(1)
+                vision_tokens = input_ids[vision_start_indices + 1]
+                image_nums = (vision_tokens == image_token_id).sum()
+                video_nums = (vision_tokens == video_token_id).sum()
+                input_tokens = input_ids.tolist()
+                llm_pos_ids_list: list = []
+                st = 0
+                remain_images, remain_videos = image_nums, video_nums
+                for _ in range(image_nums + video_nums):
+                    if image_token_id in input_tokens and remain_images > 0:
+                        ed_image = input_tokens.index(image_token_id, st)
+                    else:
+                        ed_image = len(input_tokens) + 1
+                    if video_token_id in input_tokens and remain_videos > 0:
+                        ed_video = input_tokens.index(video_token_id, st)
+                    else:
+                        ed_video = len(input_tokens) + 1
+                    if ed_image < ed_video:
+                        t, h, w = (
+                            image_grid_thw[image_index][0],
+                            image_grid_thw[image_index][1],
+                            image_grid_thw[image_index][2],
+                        )
+                        image_index += 1
+                        remain_images -= 1
+                        ed = ed_image
+
+                    else:
+                        t, h, w = (
+                            video_grid_thw[video_index][0],
+                            video_grid_thw[video_index][1],
+                            video_grid_thw[video_index][2],
+                        )
+                        video_index += 1
+                        remain_videos -= 1
+                        ed = ed_video
+                    llm_grid_t, llm_grid_h, llm_grid_w = (
+                        t.item(),
+                        h.item() // spatial_merge_size,
+                        w.item() // spatial_merge_size,
+                    )
+                    text_len = ed - st
+
+                    st_idx = (
+                        llm_pos_ids_list[-1].max() + 1
+                        if len(llm_pos_ids_list) > 0
+                        else 0
+                    )
+                    llm_pos_ids_list.append(
+                        torch.arange(text_len).view(1, -1).expand(3, -1) + st_idx
+                    )
+
+                    # t_index is always 0 because llm_grid_t is always 1 (we use timestamps to encode the temporal information for videos)
+                    t_index = (
+                        torch.arange(llm_grid_t)
+                        .view(-1, 1)
+                        .expand(-1, llm_grid_h * llm_grid_w)
+                        .flatten()
+                    )
+                    h_index = (
+                        torch.arange(llm_grid_h)
+                        .view(1, -1, 1)
+                        .expand(llm_grid_t, -1, llm_grid_w)
+                        .flatten()
+                    )
+                    w_index = (
+                        torch.arange(llm_grid_w)
+                        .view(1, 1, -1)
+                        .expand(llm_grid_t, llm_grid_h, -1)
+                        .flatten()
+                    )
+                    llm_pos_ids_list.append(
+                        torch.stack([t_index, h_index, w_index]) + text_len + st_idx
+                    )
+                    st = ed + llm_grid_t * llm_grid_h * llm_grid_w
+
+                if st < len(input_tokens):
+                    st_idx = (
+                        llm_pos_ids_list[-1].max() + 1
+                        if len(llm_pos_ids_list) > 0
+                        else 0
+                    )
+                    text_len = len(input_tokens) - st
+                    llm_pos_ids_list.append(
+                        torch.arange(text_len).view(1, -1).expand(3, -1) + st_idx
+                    )
+
+                llm_positions = torch.cat(llm_pos_ids_list, dim=1).reshape(3, -1)
+                position_ids[..., i, attention_mask[i] == 1] = llm_positions.to(
+                    position_ids.device
+                )
+                mrope_position_deltas.append(
+                    llm_positions.max() + 1 - len(total_input_ids[i])
+                )
+            mrope_position_deltas = torch.tensor(
+                mrope_position_deltas, device=input_ids.device
+            ).unsqueeze(1)
+            return position_ids, mrope_position_deltas
+        else:
+            if attention_mask is not None:
+                position_ids = attention_mask.long().cumsum(-1) - 1
+                position_ids.masked_fill_(attention_mask == 0, 1)
+                position_ids = (
+                    position_ids.unsqueeze(0)
+                    .expand(3, -1, -1)
+                    .to(attention_mask.device)
+                )
+                max_position_ids = position_ids.max(0, keepdim=False)[0].max(
+                    -1, keepdim=True
+                )[0]
+                mrope_position_deltas = max_position_ids + 1 - attention_mask.shape[-1]
+            else:
+                position_ids = (
+                    torch.arange(input_ids.shape[1], device=input_ids.device)
+                    .view(1, 1, -1)
+                    .expand(3, input_ids.shape[0], -1)
+                )
+                mrope_position_deltas = torch.zeros(
+                    [input_ids.shape[0], 1],
+                    device=input_ids.device,
+                    dtype=input_ids.dtype,
+                )
+
+            return position_ids, mrope_position_deltas
+
     def _process_single_sample(
         self,
         conversation: "HFVLMDataPacker.Payload",
@@ -371,6 +537,20 @@ class HFVLMDataPacker(DataPacker):
             result_dict["batch_num_images"] = inputs["batch_num_images"]
         else:
             result_dict["batch_num_images"] = None
+
+        # position_ids: (3, 1, seq_len)
+        # Only for Qwen3VLMOE
+        position_ids, _ = self._get_rope_index(
+            input_ids=torch.tensor(input_ids).unsqueeze(0).clone(),
+            image_grid_thw=torch.tensor(result_dict.get("image_grid_thw"))
+            if "image_grid_thw" in result_dict
+            else None,
+            video_grid_thw=torch.tensor(result_dict.get("video_grid_thw"))
+            if "video_grid_thw" in result_dict
+            else None,
+            attention_mask=None,
+        )
+        result_dict["position_ids"] = position_ids.clone()
 
         return result_dict
 
@@ -570,6 +750,15 @@ class HFVLMDataPacker(DataPacker):
             batch["logprob_masks"]
         ), "The length of input_ids, logprob_masks should be the same"
 
+        padded_tensors = []
+        for sample in processed_samples:
+            pad_length = computed_max_len - sample["position_ids"].shape[2]
+            padded_tensor = torch.nn.functional.pad(
+                sample["position_ids"], (0, pad_length), "constant", 1
+            )
+            padded_tensors.append(padded_tensor)
+        batch["position_ids"] = torch.cat(padded_tensors, dim=1)
+
         return batch
 
     def get_policy_input(
@@ -587,7 +776,9 @@ class HFVLMDataPacker(DataPacker):
             add_generation_prompt=add_generation_prompt,
         )
 
-        return_dict = {}
+        return_dict = {
+            "position_ids": x["position_ids"],
+        }
         if "pixel_values_videos" in x:
             return_dict["pixel_values_videos"] = x["pixel_values_videos"]
         else:
@@ -652,7 +843,19 @@ class HFVLMDataPacker(DataPacker):
         completion_ids = []
         if rollout_output:
             completion_ids = self.tokenizer(rollout_output).input_ids  # Don't pad yet
-
+            # recompute position_ids
+            # position_ids: (3, 1, seq_len)
+            position_ids, _ = self._get_rope_index(
+                input_ids=torch.tensor(input_ids + completion_ids).unsqueeze(0).clone(),
+                image_grid_thw=torch.tensor(x.get("image_grid_thw"))
+                if "image_grid_thw" in x
+                else None,
+                video_grid_thw=torch.tensor(x.get("video_grid_thw"))
+                if "video_grid_thw" in x
+                else None,
+                attention_mask=None,
+            )
+            return_dict["position_ids"] = position_ids.clone()
         return_dict["input_ids"] = input_ids + completion_ids
 
         return_dict["logprob_masks"] = (
