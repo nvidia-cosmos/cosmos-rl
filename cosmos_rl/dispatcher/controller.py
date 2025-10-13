ba6c0bc0cfc0863f663d8b2cd3788df16ff94df5
lms <maoshengl@nvidia.com>
2025-10-13 06:28:45 +0000

    raise error.

--- cosmos_rl/dispatcher/controller.py
+++ cosmos_rl/dispatcher/controller.py
@@ -575,22 +575,22 @@ class Controller:
             weight_version_for_current_batch = (
                 num_of_valid_prompts_consumed // global_batch_size
             )
-            # check if for current weight version, we have reached the upper limit of retries to generate enough samples.
-            if self.config.train.train_policy.max_retry_for_on_policy > 0:
-                if (
-                    weight_version_for_current_batch in self.weight_version_to_batch_num
-                    and self.weight_version_to_batch_num[
-                        weight_version_for_current_batch
-                    ]
-                    > self.config.train.train_policy.max_retry_for_on_policy
-                ):
-                    logger.error(
-                        f"[Controller] After {self.config.train.train_policy.max_retry_for_on_policy} retries, samples for weight version {weight_version_for_current_batch} are still not enough. May be the dataset is too difficult for current model? Or you could also set the `max_retry_for_on_policy` to 0 to always retry."
-                    )
+
+            # record the number of batches for current weight version
             if weight_version_for_current_batch not in self.weight_version_to_batch_num:
                 self.weight_version_to_batch_num[weight_version_for_current_batch] = 1
             else:
                 self.weight_version_to_batch_num[weight_version_for_current_batch] += 1
+
+            # check if for current weight version, we have reached the upper limit of retries to generate enough samples.
+            if self.config.train.train_policy.max_retry_for_on_policy > 0:
+                if (
+                    self.weight_version_to_batch_num[weight_version_for_current_batch]
+                    > self.config.train.train_policy.max_retry_for_on_policy
+                ):
+                    raise RuntimeError(
+                        f"[Controller] After {self.config.train.train_policy.max_retry_for_on_policy} retries, samples for weight version {weight_version_for_current_batch} are still not enough. May be the dataset is too difficult for current model? Or you could also set the `max_retry_for_on_policy` to 0 or negative to always retry."
+                    )
 
             for i in range(current_fetch_count):
                 # get_batched_prompt is called in single thread, so we use `consumed_samples_num` to calculate the weight version.
