d642e7622aaac69f011d81c392c98a6bc71461fe
liang.feng <liangf@nvidia.com>
2025-10-31 03:16:24 +0000

    Not optimize change

--- cosmos_rl/policy/trainer/grpo_trainer.py
+++ cosmos_rl/policy/trainer/grpo_trainer.py
@@ -983,49 +983,35 @@ class GRPOTrainer(Trainer):
         # Add nccl allreduce operations for all parameters and necessary states.
         """
         with torch.cuda.stream(self.train_stream):
-            if (
-                self.inter_policy_nccl.is_single_peer.is_set()
-                and not self.parallel_dims.pp_enabled
-            ):
-                all_params = [
-                    p
-                    for m in [model for model in self.model_parts if model is not None]
-                    for p in m.parameters()
-                ]
-                grad_norm = torch.nn.utils.clip_grad_norm_(
-                    all_params, max_norm=self.config.train.optm_grad_norm_clip
-                )
-            else:
-                for model_part in self.model_parts:
-                    # Model part may use same physical mesh for different logical mesh,
-                    # which is not supported by DTensor operands like `torch.nn.utils.get_total_norm`
-                    # So we need to do allreduce for each model part
-                    if model_part is not None:
-                        dist_util.gradient_reduce_across_dp_replicas_(
-                            [p for p in model_part.parameters()], self.inter_policy_nccl
-                        )
+            for model_part in self.model_parts:
+                # Model part may use same physical mesh for different logical mesh,
+                # which is not supported by DTensor operands like `torch.nn.utils.get_total_norm`
+                # So we need to do allreduce for each model part
+                if model_part is not None:
+                    dist_util.gradient_reduce_across_dp_replicas_(
+                        [p for p in model_part.parameters()], self.inter_policy_nccl
+                    )
 
-                """
-                Compute the global grad norm on all parameters and then apply
-                gradient clipping using the global grad norm.
-                """
-                # Must pass empty list even if model_part is None,
-                # GradNorm across pp stages will fail if some rank does not join the barrier
-                all_params = [
-                    p
-                    for m in [model for model in self.model_parts if model is not None]
-                    for p in m.parameters()
-                ]
-                grad_norm = dist_util.gradient_norm_clipping(
-                    all_params,
-                    self.config.train.optm_grad_norm_clip,
-                    foreach=True,
-                    pp_mesh=self.parallel_dims.mesh["pp"]
-                    if self.parallel_dims.pp_enabled
-                    else None,
-                    return_norm_only=(self.config.train.optm_grad_norm_clip <= 0.0),
-                )
-
+            """
+            Compute the global grad norm on all parameters and then apply
+            gradient clipping using the global grad norm.
+            """
+            # Must pass empty list even if model_part is None,
+            # GradNorm across pp stages will fail if some rank does not join the barrier
+            all_params = [
+                p
+                for m in [model for model in self.model_parts if model is not None]
+                for p in m.parameters()
+            ]
+            grad_norm = dist_util.gradient_norm_clipping(
+                all_params,
+                self.config.train.optm_grad_norm_clip,
+                foreach=True,
+                pp_mesh=self.parallel_dims.mesh["pp"]
+                if self.parallel_dims.pp_enabled
+                else None,
+                return_norm_only=(self.config.train.optm_grad_norm_clip <= 0.0),
+            )
             self.optimizers.step()
             self.optimizers.zero_grad()
         return grad_norm
