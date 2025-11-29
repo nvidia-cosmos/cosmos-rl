cf416d160a48a2c79c5e5ad0ec56bcfd27fc9def
maoshengl <maoshengl@nvidia.com>
2025-11-29 02:01:14 +0000

    update

--- tests/test_policy_variant.py
+++ tests/test_policy_variant.py
@@ -13,14 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import unittest
 import os
 import subprocess
 import sys
-import util
-import toml
+from cosmos_rl.utils import util
 import tempfile
+import toml
 
 
 class TestGspo(unittest.TestCase):
