85e0bc43af6396384b17bea26891029e1f0e6624
Bingjie Zhou <bingjiez@nvidia.com>
2025-12-04 10:35:31 -0800

    refactor: remove non-training related tasks

--- cosmos_rl/inference/__init__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-from .infer_microservices import CosmosInferenceMicroservice
-
-__all__ = [
-    "CosmosInferenceMicroservice",
-]
-
