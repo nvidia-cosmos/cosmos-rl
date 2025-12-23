244ac7ad30e3b0006317ae7c464d6b970a357794
maoshengl <maoshengl@nvidia.com>
2025-12-23 03:13:27 +0000

    keep seed default value aligned with vLLM.

--- cosmos_rl/policy/config/__init__.py
+++ cosmos_rl/policy/config/__init__.py
@@ -1234,7 +1234,8 @@ class RolloutConfig(BaseModel):
         choices=["none", "fp8"],
     )
 
-    seed: Optional[int] = Field(default=None, description="random seed for rollout.")
+    # This default seed is aligned with vLLM
+    seed: int = Field(default=0, description="random seed for rollout.")
 
     sampling_config: SamplingConfig = Field(default_factory=SamplingConfig)
 
