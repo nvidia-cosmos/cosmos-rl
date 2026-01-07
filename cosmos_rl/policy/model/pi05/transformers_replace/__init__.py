651cd2fe01fac071d01d5a70ae45ed8232e0ad27
Liang Hao <haolia@nvidia.com>
2026-01-07 11:13:10 +0000

    fix import

--- cosmos_rl/policy/model/pi05/transformers_replace/__init__.py
+++ cosmos_rl/policy/model/pi05/transformers_replace/__init__.py
@@ -1,8 +1,16 @@
 # transformers_replace - Local model implementations for pi05
 
-# from .configuration_gemma import GemmaConfig
-# from .configuration_paligemma import PaliGemmaConfig
-# from .modeling_gemma import *
-# from .modeling_paligamma import PaliGemmaForConditionalGeneration
+from .configuration_gemma import GemmaConfig
+from .configuration_paligemma import PaliGemmaConfig
+from .modeling_gemma import GemmaForCausalLM
+from .modeling_paligamma import PaliGemmaForConditionalGeneration
 
 from . import modeling_gemma as modeling_gemma
+
+__all__ = [
+    "GemmaConfig",
+    "GemmaForCausalLM",
+    "PaliGemmaConfig",
+    "PaliGemmaForConditionalGeneration",
+    "modeling_gemma",
+]
