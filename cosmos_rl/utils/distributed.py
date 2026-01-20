947566c5945cb782e60b8f9d7568500832459d48
maoshengl <maoshengl@nvidia.com>
2026-01-20 05:17:27 +0000

    assert for cuda

--- cosmos_rl/utils/distributed.py
+++ cosmos_rl/utils/distributed.py
@@ -56,6 +56,7 @@ from torch._utils import _get_available_device_type, _get_device_module
 
 def get_device_info() -> tuple[str, torch.device]:
     device_type = _get_available_device_type() or "cuda"
+    assert device_type == "cuda", "Only CUDA device is supported now."
     device_module = _get_device_module(device_type)  # default device_module:torch.cuda
     return device_type, device_module
 
