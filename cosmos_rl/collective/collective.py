28441dd114332195e27e658cf639a8596c765dfa
Maosheng Liao <maoshengl@nvidia.com>
2026-01-21 21:45:00 -0800

    initial commit

---
+++ cosmos_rl/collective/collective.py
@@ -0,0 +1,47 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from queue import Queue
+
+
+class Collective:
+    """
+    A manager for collective operations. Abstract the send/recv operations for
+    IPC and NCCL.
+    Only send and recv operations are supported.
+    """
+
+    def __init__(self):
+        """
+        Initialize the CollectiveManager.
+        """
+        self.peers = []
+
+        # For IPC communication
+        self.ipc_queue = Queue()
+        self.nccl_comm = {}
+
+    def send(self):
+        """
+        Send data to a peer.
+        """
+        pass
+
+    def recv(self):
+        """
+        Receive data from a peer.
+        """
+        pass
